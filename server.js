6392676c5884c16e415946ed173500f345093a03
Okkun555 <y.okuno.btm@gmail.com>
2023-04-01 10:19:59 +0900

    ルーティングの追加

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const api = require("./routes/api");
 const app = express();
 const port = 3001;
 
 app.use(express.static("public"));
+app.use("/api", api);
 
 app.get("/", (req, res, next) => {
   res.end("Top Page");
